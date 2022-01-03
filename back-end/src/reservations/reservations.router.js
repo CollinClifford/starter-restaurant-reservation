8dbd2b034bcaed8442fc5c8f1db403fe53ae4137
Collin Clifford <collinclifford@ymail.com>
2022-01-03 11:49:46 -0800

    enabled cors

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -7,24 +7,26 @@
 const router = require("express").Router();
 const controller = require("./reservations.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
 
-router.route("/:reservationId/mobile_phone");
+router.route("/:reservationId/mobile_phone").all(cors());
 
 router
   .route("/:reservationId/status")
+  .all(cors())
   .put(controller.updateStatus)
   .all(methodNotAllowed);
 
-router.route("/:reservationId/seat").get(controller.read).all(methodNotAllowed);
+router.route("/:reservationId/seat").all(cors()).get(controller.read).all(methodNotAllowed);
 
 router
-  .route("/:reservationId")
+  .route("/:reservationId").all(cors())
   .get(controller.read)
   .put(controller.update)
   .all(methodNotAllowed);
 
 router
-  .route("/")
+  .route("/").all(cors())
   .get(controller.list)
   .post(controller.create)
   .all(methodNotAllowed);
--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -1,17 +1,18 @@
 const router = require("express").Router();
 const controller = require("./tables.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors")
 
 router
-  .route("/:tableId/seat")
+  .route("/:tableId/seat").all(cors())
   .put(controller.update)
   .delete(controller.delete)
   .all(methodNotAllowed);
 
-router.route("/:tableId").get(controller.read).all(methodNotAllowed);
+router.route("/:tableId").all(cors()).get(controller.read).all(methodNotAllowed);
 
 router
-  .route("/")
+  .route("/").all(cors())
   .get(controller.list)
   .post(controller.create)
   .all(methodNotAllowed);
